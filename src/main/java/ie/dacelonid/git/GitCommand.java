55b2bb278853c6cbbb1bc3a412db1126fb09d434
Ken O'Neill <kenneth.oneill@gmail.com>
2025-06-23 12:32:32 +0100

    Added RepoUtils

--- src/main/java/ie/dacelonid/git/GitCommand.java
+++ src/main/java/ie/dacelonid/git/GitCommand.java
@@ -1,15 +1,13 @@
 package ie.dacelonid.git;
 
 import ie.dacelonid.git.exceptions.*;
-import ie.dacelonid.git.plumbing.BlobUtils;
+import ie.dacelonid.git.plumbing.RepoUtils;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static ie.dacelonid.git.plumbing.BlobUtils.*;
-import static ie.dacelonid.git.utils.FileUtilities.*;
+import static ie.dacelonid.git.plumbing.RepoUtils.*;
 
 public class GitCommand {
 
@@ -32,10 +30,6 @@ public class GitCommand {
 
     }
 
-    private File getGitRootDirectory(Path currentDirectory) throws GitRepoNotInitialized {
-        return new File(currentDirectory.toFile(), ".git");
-    }
-
     private void initializeRepo(File gitRootDir) throws GitExceptions {
         if(gitRootDir.exists()){
             throw new GitRepoAlreadyExists();
@@ -43,24 +37,6 @@ public class GitCommand {
         createGitDirectoryStructure(gitRootDir);
         createHeadFile(gitRootDir);
         System.out.println("Initialized git directory");
-    }
-
-    private void createGitDirectoryStructure(File gitDirectory) throws GitCouldNotCreateDirectoryException {
-        createDirectory(gitDirectory);
-        createDirectory(new File(gitDirectory, "refs"));
-        createDirectory(new File(gitDirectory, "objects"));
-    }
-
-    private void createHeadFile(File gitDirectory) throws GitCouldNotCreateHead {
-        final File head = new File(gitDirectory, "HEAD");
-        try {
-            boolean fileCreated = head.createNewFile();
-            if (!fileCreated)
-                throw new GitCouldNotCreateHead();
-            Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
-        } catch (IOException e) {
-            throw new GitCouldNotCreateHead();
-        }
     }
 
     private static String getCommandTarget(String[] args) {
---
+++ src/main/java/ie/dacelonid/git/plumbing/RepoUtils.java
@@ -0,0 +1,36 @@
+package ie.dacelonid.git.plumbing;
+
+import ie.dacelonid.git.exceptions.GitCouldNotCreateDirectoryException;
+import ie.dacelonid.git.exceptions.GitCouldNotCreateHead;
+import ie.dacelonid.git.exceptions.GitRepoNotInitialized;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static ie.dacelonid.git.utils.FileUtilities.createDirectory;
+
+public class RepoUtils {
+    public static void createGitDirectoryStructure(File gitDirectory) throws GitCouldNotCreateDirectoryException {
+        createDirectory(gitDirectory);
+        createDirectory(new File(gitDirectory, "refs"));
+        createDirectory(new File(gitDirectory, "objects"));
+    }
+
+    public static void createHeadFile(File gitDirectory) throws GitCouldNotCreateHead {
+        final File head = new File(gitDirectory, "HEAD");
+        try {
+            boolean fileCreated = head.createNewFile();
+            if (!fileCreated)
+                throw new GitCouldNotCreateHead();
+            Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
+        } catch (IOException e) {
+            throw new GitCouldNotCreateHead();
+        }
+    }
+
+    public static File getGitRootDirectory(Path currentDirectory) throws GitRepoNotInitialized {
+        return new File(currentDirectory.toFile(), ".git");
+    }
+}
